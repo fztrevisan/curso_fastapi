f583bb4148fea577eaae2a367697127a5a0080f6
Fernando Trevisan <trevisan.fernando@gmail.com>
2025-02-08 12:03:39 -0300

    FEAT creates pydantic schemas to validate users

--- fast_zero/schemas.py
+++ fast_zero/schemas.py
@@ -1,5 +1,28 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class Message(BaseModel):
     message: str
+
+
+class UserSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+
+# Temporario para a aula 03
+class UserDB(UserSchema):
+    id: int
+
+
+class UserPublic(BaseModel):
+    """User public data with no password"""
+
+    id: int
+    username: str
+    email: EmailStr
+
+
+class UserList(BaseModel):
+    users: list[UserPublic]
