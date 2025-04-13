f7e5dfb99a61700a95cdd4a291362c294972faf3
Fernando Trevisan <trevisan.fernando@gmail.com>
2025-04-13 19:02:47 -0300

    FEAT add error handling tests for user creation and read user tests with DB

--- tests/test_app.py
+++ tests/test_app.py
@@ -37,22 +37,51 @@ def test_create_user(client: TestClient):
     }
 
 
-# def test_read_user(client: TestClient):
-#     response = client.get('/users/1')
+def test_create_user_error_username_exists(client: TestClient, user):
+    response = client.post(
+        url='/users/',
+        json={
+            'username': user.username,
+            'password': 'testpwd',
+            'email': 'testemail@email.com',
+        },
+    )
 
-#     assert response.status_code == HTTPStatus.OK
-#     assert response.json() == {
-#         'id': 1,
-#         'username': 'testusername',
-#         'email': 'test@email.com',
-#     }
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {
+        'detail': 'Username already exists',
+    }
 
 
-# def test_read_user_not_found(client: TestClient):
-#     response = client.get('/users/999')
+def test_create_user_error_email_exists(client: TestClient, user):
+    response = client.post(
+        url='/users/',
+        json={
+            'username': 'testusername',
+            'password': 'testpwd',
+            'email': user.email,
+        },
+    )
 
-#     assert response.status_code == HTTPStatus.NOT_FOUND
-#     assert response.json() == {'detail': 'User not found'}
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {
+        'detail': 'Email already exists',
+    }
+
+
+def test_read_user(client: TestClient, user):
+    user_public = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == user_public
+
+
+def test_read_user_not_found(client: TestClient):
+    response = client.get('/users/999')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
 
 
 def test_read_users(client: TestClient):
