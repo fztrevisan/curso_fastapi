1e5d1dd768d374c0a068c15655529d4b53cc1bdf
Fernando Trevisan <trevisan.fernando@gmail.com>
2025-05-14 20:24:00 -0300

    REFACTOR update settings to include additional parameters for environment configuration

--- fast_zero/settings.py
+++ fast_zero/settings.py
@@ -5,6 +5,11 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file='.env',
         env_file_encoding='utf-8',
+        extra='ignore'
     )
-
+    # extra='ignore' serve para ignorar novos params no .env
+    # que não estão mapeados abaixo, senão dá exception
     DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
