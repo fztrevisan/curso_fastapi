c0f031a21964552a6489e92e67dfbed00afbd9a1
Fernando Trevisan <trevisan.fernando@gmail.com>
2026-02-17 17:37:48 -0300

    FEAT implement rate limiting for all routes using SlowAPI middleware

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -3,10 +3,22 @@ from pathlib import Path
 
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.middleware import SlowAPIMiddleware
+from slowapi.errors import RateLimitExceeded
 
 from fast_zero.routers import auth, todo, users
 
+
+limiter = Limiter(key_func=get_remote_address, default_limits=["5/minute"])
+
 app = FastAPI()
+# adds rate limit to all routes in the app
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.add_middleware(SlowAPIMiddleware)
+
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(todo.router)
