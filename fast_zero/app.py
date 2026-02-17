9248339974dacbec645ecca6f927ada85f40de5d
Fernando Trevisan <trevisan.fernando@gmail.com>
2026-02-17 16:15:14 -0300

    REFACTOR root endpoint to serve HTML file instead of JSON response

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,10 +1,10 @@
 from http import HTTPStatus
+from pathlib import Path
 
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse
 
 from fast_zero.routers import auth, todo, users
-from fast_zero.schemas import Message
 
 app = FastAPI()
 app.include_router(users.router)
@@ -12,21 +12,7 @@ app.include_router(auth.router)
 app.include_router(todo.router)
 
 
-@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
+@app.get('/', status_code=HTTPStatus.OK, response_class=FileResponse)
 def read_root():
-    return {'message': 'Olá Mundo!'}
-
-
-# response_class informs FastAPI that the response will be diffent from JSON
-@app.get('/formatted', status_code=HTTPStatus.OK, response_class=HTMLResponse)
-def read_formatted():
-    return """
-        <html lang="pt-br">
-        <head>
-            <title>Meu olá mundo</title>
-        </head>
-        <body>
-            <h1>Olá mundo</h1>
-        </body>
-        </html>
-    """
+    html_path = Path(__file__).parent / 'static' / 'index.html'
+    return FileResponse(html_path, media_type='text/html')
