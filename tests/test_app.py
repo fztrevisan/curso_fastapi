905385dc2171101b73148920e46c29d9ef1be4ec
Fernando Trevisan <trevisan.fernando@gmail.com>
2026-02-17 16:19:25 -0300

    FIX remove test for /formatted route in test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,13 +10,6 @@ def test_root_deve_retornar_ok_e_ola_mundo(client: TestClient):
     assert response.json() == {'message': 'Olá Mundo!'}
 
 
-def test_formatted_retorna_ok_e_ola_mundo_em_html(client: TestClient):
-    response = client.get('/formatted')
-
-    assert response.status_code == HTTPStatus.OK
-    assert 'Olá mundo' in response.text
-
-
 def test_login_for_access_token_bad_request(client: TestClient, session):
     data = {'username': 'non-existent@email.com', 'password': '123456'}
     response = client.post('/auth/token', data=data)
